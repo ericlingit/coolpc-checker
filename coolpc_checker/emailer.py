36208784c12238ad601dd237adea36a423be2873
ericlingit <ericlingit@users.noreply.github.com>
2020-11-19 19:39:25 +0900

    Use google's official api

--- coolpc_checker/emailer.py
+++ coolpc_checker/emailer.py
@@ -1,32 +1,83 @@
-import smtplib
-import ssl
+import pickle
+import os.path
+import base64
+from email.mime.text import MIMEText
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-# Load credentials
-with open('./coolpc_checker/credentials.txt') as fh:
-    email, password = fh.read().split()
+SCOPES = ['https://mail.google.com/']
 
 
-# https://realpython.com/python-send-email
-smtp_server = 'smtp.gmail.com'
-port = 587  # For starttls
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
 
-context = ssl.create_default_context()
-msg = '''Subject: test
+    Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
 
-hello world
-'''
+    Returns:
+        An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode('utf8')).decode()}
 
-# Log in and send email
-try:
-    with smtplib.SMTP(smtp_server, port, timeout=5) as server:
-        server.starttls(context=context)
-        server.login(email, password)
 
-        # Send email
-        server.sendmail(
-            from_addr=email,
-            to_addrs=email,  # Send to self
-            msg=msg
-        )
-except Exception as e:
-    print(e)
+def send_message(service, user_id, message):
+    """Send an email message.
+
+    Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me"
+        can be used to indicate the authenticated user.
+        message: Message to be sent.
+
+    Returns:
+        Sent Message.
+    """
+    try:
+        message = (service
+            .users()
+            .messages()
+            .send(userId=user_id, body=message)
+            .execute())
+        print(f'Message Id: {message["id"]}')
+        return message
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+
+def main():
+    """Shows basic usage of the Gmail API."""
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail API
+    m = create_message('sender@gmail.com', 'receiver@gmail.com', 'subject', 'hello world')
+    send_message(service, 'me', m)
+
+if __name__ == '__main__':
+    main()
